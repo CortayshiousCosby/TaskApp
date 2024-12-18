b99320e1cccb20da76be0b7bf719072e73fbff01
Joshua Bertrand <107531452+PremierHealthSolutions@users.noreply.github.com>
2024-12-18 11:02:24 -0600

    Added 'Add Task' button

--- resources/js/Pages/Task/Index.tsx
+++ resources/js/Pages/Task/Index.tsx
@@ -1,19 +1,28 @@
-import { Box, Button, HStack, Spacer, VStack } from "@chakra-ui/react";
-import { FC } from "react";
-import TaskTable from "./components/TaskTable";
-import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { ChevronLeftIcon, SmallAddIcon } from "@chakra-ui/icons";
+import { Button, HStack, Spacer, VStack } from "@chakra-ui/react";
 import { Link } from "@inertiajs/react";
+import { FC } from "react";
+import { ModalDisplay } from "../../components/Utility";
+import TaskFieldGroup from "./components/TaskFieldGroup";
+import TaskTable from "./components/TaskTable";
 
 const Index: FC = () => {
     return (
         <VStack align="stretch">
             <HStack>
-                <Box>
-                    <Button leftIcon={<ChevronLeftIcon />} as={Link} href="/">
-                        Back
-                    </Button>
-                    <Spacer />
-                </Box>
+                <Button leftIcon={<ChevronLeftIcon />} as={Link} href="/">
+                    Back
+                </Button>
+                <Spacer />
+                <ModalDisplay
+                    triggerButton={
+                        <Button colorScheme="green" leftIcon={<SmallAddIcon />}>
+                            Add Task
+                        </Button>
+                    }
+                >
+                    <TaskFieldGroup title="Create Task" method="POST" />
+                </ModalDisplay>
             </HStack>
             <TaskTable />
         </VStack>
